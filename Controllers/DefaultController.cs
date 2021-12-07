24ee4f06e6c71a1019e4aebe71769e670a83c56e
Rajanikant <rajanikantpawar31@gmail.com>
2021-12-07 20:02:11 +0530

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            if (hour >= 5 && hour < 12)
+                ViewBag.Greeting = "Good Morning!";
+            else if (hour >= 12 && hour < 16)
+                ViewBag.Greeting = "Good Afternoon!";
+            else if (hour >= 16 && hour < 20)
+                ViewBag.Greeting = "Good Evening!";
+            else
+                ViewBag.Greeting = "It's Late, Please go to sleep ";
+            return View();
+        }
+        public ActionResult CheckDifference()
+        {
+            Session["Session1"] = "Until Browser Closes";
+            TempData["FortheFullRequest"] = "For the Full Request";
+            ViewData["MyData"] = "ControllertoView";
+            ViewBag.MyBag = "SingleRequest Either ControllertoView or ControllertoController";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string strTempData = Convert.ToString(TempData["FortheFullRequest"]);
+            string str2Session = Session["Session1"].ToString();
+            string str3ViewData = Convert.ToString(ViewData["MyData"]);
+            string str4ViewBag = ViewBag.MyBag;
+            return View();
+        }
+
+  
+    }
+}
